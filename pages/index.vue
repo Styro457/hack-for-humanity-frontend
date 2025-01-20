bfeab32f217dd89d1516cd5144bbd6ad84991458
Styro <69657780+Styro457@users.noreply.github.com>
2025-01-21 01:52:26 +0200

    Create base home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+const value = ref(null)
+const items = ref([])
+
+const search = (event) => {
+  items.value = [...Array(10).keys()].map((item) => event.query + '-' + item);
+}
 
 </script>
 
 <template>
-<h1>Home</h1>
+  <div class="title">
+    <h1 class="bold-800">Discover Amazing<br>University Courses</h1>
+    <h2>
+      Lorem ipsum dolor sit amet, consectetur elit. Sed eget est quis arcu.<br>
+      Donec elementum congue nulla, ac sollicitudin erat tristique eu.
+    </h2>
+  </div>
+  <div class="card flex flex-wrap justify-center gap-4">
+    <IconField>
+      <InputIcon class="pi pi-search" />
+      <InputText v-model="value" :suggestions="items" @complete="search" autocomplete="on"/>
+    </IconField>
+  </div>
+
+
 </template>
 
 <style scoped>
