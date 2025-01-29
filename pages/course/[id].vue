e435276e1c3df14b3a0cabb6f30ef9675e919f58
Styro <69657780+Styro457@users.noreply.github.com>
2025-01-29 16:33:55 +0200

    Create base of Course page

--- pages/course/[id].vue
+++ pages/course/[id].vue
@@ -4,9 +4,30 @@ const id = route.params.id
 </script>
 
 <template>
-<h1>Some Course: {{ id }}</h1>
+  <div class="container">
+    <span class="subtitle content">CS302</span>
+  </div>
+  <div class="course-info">
+    <h2 class="bold-700">Advanced Algorithms and Data Structures</h2>
+    <div class="columns">
+      <div class="column first">
+          <p class="text"><i class="pi pi-search"></i> Northbridge University, UK</p>
+      </div>
+      <div class="column second">
+        <p class="text"><i class="pi pi-book"></i> Computer Science, Software Engineering,
+          Information Technology </p>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
+.course-info {
+  width: 70%;
+}
 
+.first {
+  width: 60%;
+}
 </style>
