25fcd7052f58e33a950eef89bcbb51b4da556022
Styro <69657780+Styro457@users.noreply.github.com>
2025-02-01 19:16:06 +0200

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,12 +28,21 @@ export default defineNuxtConfig({
     host: '127.0.0.1',
     port: 3001
   },
-  head: {
-    meta: [
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-      }
-    ]
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        }
+      ],
+      link: [
+        {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+        {rel: 'apple-touch-icon', href: '/apple-icon-180x180.png'},
+        {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
+        {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
+        {rel: 'manifest', href: '/site.webmanifest'}
+      ]
+    }
   }
 })
