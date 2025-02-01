08d5e9f48a0d2ae6b65ffb0cfb2f87d1ac01c5cc
Styro <69657780+Styro457@users.noreply.github.com>
2025-02-01 18:30:45 +0200

    Update RequireLogin.vue

--- components/RequireLogin.vue
+++ components/RequireLogin.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+
 const router = useRouter();
 const route = useRoute();
 const redirectPath = ref('/');
 
-onMounted(() => {
-  if (!isConnected()) {
+
+async function handleSession() {
+  try {
+    const response = await $fetch(getAPI() + '/users/get_session', {
+      method: 'GET',
+      credentials: 'include',
+    });
+    if(response.message === "Valid") {
+    } else {
+      redirectPath.value = route.fullPath;
+      router.push({ path: '/login', query: { redirect: redirectPath.value } });
+    }
+  } catch (err) {
     redirectPath.value = route.fullPath;
     router.push({ path: '/login', query: { redirect: redirectPath.value } });
+
   }
+}
+onMounted(() => {
+  handleSession();
 });
 </script>
 
